e1f301a49a7ddb3c5d21687a51a22109915aa675
Hotel Management System <samikshadaronde@gmail.com>
2026-04-09 17:58:45 +0530

    fixed mongodb connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -17,10 +17,11 @@ import "dotenv/config";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+    await mongoose.connect(process.env.MONGODB_ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
     console.log("Database Connected");
   } catch (error) {
     console.error("Database connection failed:", error.message);
