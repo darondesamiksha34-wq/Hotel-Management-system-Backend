1c95f63c170f61686b52ef7f1e7ebf3006f76265
Hotel Management System <samikshadaronde@gmail.com>
2026-04-09 17:48:24 +0530

    fixed env variable for mongodb

--- config/mongodb.js
+++ config/mongodb.js
@@ -17,10 +17,10 @@ import "dotenv/config";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_ATLAS_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log("Database Connected");
   } catch (error) {
     console.error("Database connection failed:", error.message);
